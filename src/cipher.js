339869eaf49ec03f6bb9e22f74679edaeedc36f8
unknown <p.zelada.m@gmail.com>
2019-08-25 19:33:43 -0400

    Cifrar

--- src/cipher.js
+++ src/cipher.js
@@ -5,8 +5,8 @@ for(i=0; i<text.length; i++){
 let codeAscii= text.charCodeAt(i);
 let result1=(codeAscii - 65 + number)%26 + 65;
 mssgeFinal += String.fromCharCode(result1);
-
-} return mssgeFinal
+}
+ return mssgeFinal
   },
 
   decode: (text, number) => {
@@ -14,7 +14,8 @@ let mssgeFinal=""
 for(i=0; i<text.length; i++){
   let codeAscii=text.charCodeAt(i);
   let result2=(codeAscii - 90 - number)%26 + 90;
-  mssgeFinal +=String.fromCharCode(result2);
-} return mssgeFinal
+mssgeFinal +=String.fromCharCode(result2);
+}
+return mssgeFinal
 },
 };
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ btnCipher.addEventListener('click', () => {
     let text= document.getElementById("box1").value.toUpperCase();
     let number= parseInt(document.getElementById("number1").value);
     //imprimir en box2
-    document.getElementById("box2").innerHTML = cipher.encode(text,number);
+    document.getElementById("box2").innerHTML = window.cipher.encode(text,number);
 })
 
 const btnDecipher = document.getElementById("decipher");
 btnDecipher.addEventListener('click', () =>{
     let text= document.getElementById("box1").value;
     let number=parseInt(document.getElementById("number1").value);
-    document.getElementById("box2").innerHTML = cipher.decode(text,number);
+    document.getElementById("box2").innerHTML = window.cipher.decode(text,number);
 })
 
 //boton copiar texto
