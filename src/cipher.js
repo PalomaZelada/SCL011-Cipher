88d1512aa1a241ba803c10d49e072e1729db5657
unknown <p.zelada.m@gmail.com>
2019-08-25 18:00:14 -0400

    imagen

--- src/cipher.js
+++ src/cipher.js
@@ -3,12 +3,7 @@ window.cipher = {
     let mssgeFinal=""
 for(i=0; i<text.length; i++){
 let codeAscii= text.charCodeAt(i);
-console.log(text[i]);
-//separar
 let result1=(codeAscii - 65 + number)%26 + 65;
-;
-}
-console.log(result1)
 mssgeFinal += String.fromCharCode(result1);
 
 } return mssgeFinal
@@ -18,9 +13,7 @@ mssgeFinal += String.fromCharCode(result1);
 let mssgeFinal=""
 for(i=0; i<text.length; i++){
   let codeAscii=text.charCodeAt(i);
-  console.log(text[i]);
   let result2=(codeAscii - 90 - number)%26 + 90;
-  console.log(result2)
   mssgeFinal +=String.fromCharCode(result2);
 } return mssgeFinal
 },
